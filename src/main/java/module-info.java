d450b884423bc34eb0713d6488ecc5a4ec65ae73
SahanPremarathna <sahanpremarathana2005@gmail.com>
2025-10-12 15:48:50 +0530

    lap 1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires mysql.connector.j;
+//    requires mysql.connector.j;
 
 
     opens com.example.demo to javafx.fxml;
